6374bb01923bfe7921dfefd37bf01dab6dfbdcbf
STAPLESAMS\AruPr001 <Pravin.Arun@staples.com>
2023-11-21 16:17:12 +0530

    2. Handling Get Requests

--- 4-natours/starter/public/app.js
+++ 4-natours/starter/public/app.js
@@ -1,15 +1,17 @@
 const express=require('express')
-
+const fs=require('fs')
 const app=express()
 
-app.get('/',(req,res)=>{
-    res.status(200).
-    json({message:'Hello from the server side!',app:'Natours'})
-})
+// app.get('/',(req,res)=>{
+//     res.status(200).
+//     json({message:'Hello from the server side!',app:'Natours'})
+// })
 
-app.post('/',(req,res)=>{
-    res.send('You can post to this endpoint')
-})
+// app.post('/',(req,res)=>{
+//     res.send('You can post to this endpoint')
+// })
+
+const tours=fs.readFileSync
 
 const port=3000
 app.listen(port,()=>{
