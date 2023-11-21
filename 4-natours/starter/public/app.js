5855d938d20f73cb5198cc6eaf40ace17cbc1f9b
Pravin <a.pravin3210@gmail.com>
2023-11-21 18:11:16 +0530

    2.1 Handling Get Requests, Creating tours api

--- 4-natours/starter/public/app.js
+++ 4-natours/starter/public/app.js
@@ -11,7 +11,18 @@ const app=express()
 //     res.send('You can post to this endpoint')
 // })
 
-const tours=fs.readFileSync
+const tours=fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+
+
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
+        status:'success',
+        results:tours.length,
+        data:{
+            tours
+        }
+    })
+})
 
 const port=3000
 app.listen(port,()=>{
