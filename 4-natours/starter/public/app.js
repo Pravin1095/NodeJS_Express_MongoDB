3eda100049dd218ff1b45e5493bb43174c9fd321
Pravin <a.pravin3210@gmail.com>
2023-11-22 21:43:17 +0530

    3. Post Route implementation

--- 4-natours/starter/public/app.js
+++ 4-natours/starter/public/app.js
@@ -2,6 +2,8 @@ const express=require('express')
 const fs=require('fs')
 const app=express()
 
+app.use(express.json())
+
 // app.get('/',(req,res)=>{
 //     res.status(200).
 //     json({message:'Hello from the server side!',app:'Natours'})
@@ -21,6 +23,21 @@ app.get('/api/v1/tours',(req,res)=>{
         data:{
             tours
         }
+    })
+})
+
+app.post('/api/v1/tours',(req,res)=>{
+    const newId=tours[tours.length-1].id+1
+    const newTour=Object.assign({id:newId},req.body)
+
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
+        res.status(201).json({
+            status:'success',
+            data:{
+                newTour
+            }
+        })
     })
 })
 
