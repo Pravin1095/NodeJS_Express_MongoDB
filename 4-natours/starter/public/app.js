4c1b9cea11db34faa64f98602ab19b6cb4a5c706
Pravin <a.pravin3210@gmail.com>
2023-11-23 15:16:42 +0530

    54. Responding to URL parameters

--- 4-natours/starter/public/app.js
+++ 4-natours/starter/public/app.js
@@ -26,6 +26,25 @@ app.get('/api/v1/tours',(req,res)=>{
     })
 })
 
+app.get('/api/v1/tours/:id',(req,res)=>{
+    console.log(req.params)
+
+    const id=req.params.id*1 //Multiplying a string with number return number
+    const tour=tours.find((el)=>el.id===id)
+
+    if (!tour){
+        return res.status(404).json({
+            status:'fail',
+            messsage:'INVALID ID'
+        })
+    }
+    
+    res.status(200).json({
+        status:'success',
+        data:tour
+    })
+})
+
 app.post('/api/v1/tours',(req,res)=>{
     const newId=tours[tours.length-1].id+1
     const newTour=Object.assign({id:newId},req.body)
